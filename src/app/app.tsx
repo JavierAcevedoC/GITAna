f445e5e89a07c13e7d0d963dd80c6ac26362a6f7
Javier Acevedo <javier.iacevedoc@gmail.com>
2021-02-21 20:47:25 -0300

    feat(UI): First look to create an  UI , using material-ui

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-//import 'app.scss'; // New import!!
+import { Button } from '@material-ui/core';
 import { ipcRenderer } from 'electron';
+import ListBranchs from './ui/elements/list-branchs';
 
 interface IStateApp {
     branchs: Array<string>;
@@ -10,35 +11,28 @@ class App extends Component<any ,IStateApp> {
     constructor(props: any) {
         super(props);
 
-        this.onRecieve();
-
         this.state = {
             branchs: []
         };
+
+        this.onRecieve();
     }
 
     private onRecieve(): void {
-        this.setState({
-            branchs: ['nothing','yet']
-        });
-
         ipcRenderer.send('get-branchs', '--all');
         ipcRenderer.on('return-branchs', (event: any, arg:Array<string> ) => {
 
-            console.dir(arg);
             this.setState({
                 branchs: arg
             });
             
-            this.render();
         });
     }
 
     public callPull(): void {
         ipcRenderer.send('pull', '');
         ipcRenderer.on('return-pull', (ev: any, arg: string | []) => {
-            console.dir(ev);
-            console.dir(arg);
+            //TODO set state?
         });
     }
     
@@ -48,18 +42,10 @@ class App extends Component<any ,IStateApp> {
         }
         return (
             <div style={littleStyle}>
-                <h1>Branch of this project!</h1>
-                <ul>
-                    {
-                        this.state.branchs.map( (branch: string, index: number) => {
-                            return <li key= { index }> { branch } </li>;
-                        }
-                        )
-                    } 
-                </ul>
-                <h3>By Mr.Cthulhu</h3>
-                <button onClick={ () => this.onRecieve() }>call branchs</button>
-                <button onClick={ () => this.callPull() }>call pull</button>
+                <Button color="primary" onClick={ () => this.onRecieve() }>call branchs</Button>
+                <Button color="primary" onClick={ () => this.callPull() }>call pull</Button>
+
+                <ListBranchs branchs={ this.state.branchs.map(b => b) }/>
             </div>
         );
     }
